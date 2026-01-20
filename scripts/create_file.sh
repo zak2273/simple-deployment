44043beb424fa1a4581c65ca3238c845f9bef572
zak2273 <30624277+zak2273@users.noreply.github.com>
2026-01-20 11:33:49 -0500

    fix

--- scripts/create_file.sh
+++ scripts/create_file.sh
@@ -1,8 +1,8 @@
 touch test.txt
 
 for substring in $1; do
-  result = $(( substring % 2 == 0 ))
-  if (result == 0); then
+  result = $(( $substring % 2 ))
+  if (result -eq 0); then
     echo "$substring" > test.txt
   fi
 done
